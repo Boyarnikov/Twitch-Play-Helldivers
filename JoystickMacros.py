a1acbc799b7e7a92d1ee6d1836d883945304f319
Boyar <boiarnikov.ia@phystech.edu>
2024-03-30 22:11:18 +0300

    Add commands

--- JoystickMacros.py
+++ JoystickMacros.py
@@ -3,7 +3,7 @@ import time
 import random
 
 strategy = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER
-aim = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER
+aim = "left_trigger"
 open_map = vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK
 left = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT
 up = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP
@@ -12,45 +12,31 @@ right = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT
 weapon = vg.XUSB_BUTTON.XUSB_GAMEPAD_Y
 use = vg.XUSB_BUTTON.XUSB_GAMEPAD_A
 reload = vg.XUSB_BUTTON.XUSB_GAMEPAD_X
+duck = vg.XUSB_BUTTON.XUSB_GAMEPAD_B
 
 
 gamepad = vg.VX360Gamepad()
 
-mapping = {
-    "left": left,
-    "right": right,
-    "up": up,
-    "down": down,
-    "влево": left,
-    "вправо": right,
-    "вверх": up,
-    "вниз": down,
-    "grenade": right,
-    "граната": right,
-    "хил": up,
-    "heal": up,
-    "вызов": strategy,
-    "страта": strategy,
-    "открыть": strategy,
-    "open": strategy,
-    "strat": strategy,
-    "оружие": weapon,
-    "weapon": weapon,
-    "gun": weapon,
-    "use": use,
-    "использовать": use,
-    "юзать": use,
-    "тык": use,
-    "reload": reload,
-    "перезарядка": reload,
-    "map": open_map,
-    "карта": open_map,
-    "aim": aim,
-    "прицел": aim,
-    "пуль": aim,
-    "back": None,
-    "назад": None,
+
+key_mapping = {
+    left: ["left", "влево", 'a', 'ф'],
+    right: ["right", "вправо", "grenade", "граната", 'd', 'в'],
+    up: ["up", "вверх", "heal", "хил", 'w', 'ц'],
+    down: ["down", "вниз", 'd', 'ы'],
+    strategy: ["strat", "stratagem", "вызов", "страта", "открыть", "lb"],
+    weapon: ["weapon", "оружие", "gun", "y"],
+    use: ["use", "использовать", "юзать", "тык", "a"],
+    reload: ["reload", "перезарядка", "x"],
+    open_map: ["map", "карта"],
+    aim: ["aim", "прицел", "пуль", "lt"],
+    duck: ["duck", "присесть", "b"],
+    None: ["back", "назад"]
 }
+
+mapping = dict()
+for key in key_mapping:
+    for st in key_mapping[key]:
+        mapping[st] = key
 
 toggle = {strategy, aim, None}
 
@@ -67,11 +53,19 @@ def press(key: str):
         time.sleep(random.random() * 0.1 + 0.1)
         for t in toggle:
             if t is not None:
-                gamepad.release_button(t)
+                if type(t) is str:
+                    gamepad.left_trigger(0)
+                else:
+                    gamepad.release_button(t)
+
         gamepad.update()
         time.sleep(random.random() * 0.1 + 0.1)
         if keycode is not None:
-            gamepad.press_button(keycode)
+            if type(keycode) is str:
+                gamepad.left_trigger(1)
+            else:
+                gamepad.press_button(keycode)
+
         gamepad.update()
         return
 
@@ -81,3 +75,14 @@ def press(key: str):
     time.sleep(random.random() * 0.1 + 0.1)
     gamepad.release_button(keycode)
     gamepad.update()
+
+
+if __name__ == "__main__":
+    while True:
+        time.sleep(random.random() * 0.1 + 0.1)
+        gamepad.left_trigger(1)
+        gamepad.update()
+        time.sleep(random.random() * 0.1 + 1)
+        gamepad.left_trigger(0)
+        gamepad.update()
+        time.sleep(random.random() * 0.1 + 1)
--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # TwitchPlayHelldivers
 
-Утилита для взаимодействия твич чата с джойстиком
-Комманды:
+Утилита для взаимодействия twitch чата с джойстиком
+
+Для подключения нужен app аккаунт twitch
+
+Переменные-секреты twitchAppId и twitchAppToken нужно поместить в Tokens.py
+
+Команды:
 
 ```
-D-pad:
-  left: "left", "влево"
-  right: "right", "вправо", "grenade", "граната"
-  up: "up", "вверх", "хил", "heal"
-  down: "down", "вниз"
-Toggle strategem menu: "вызов", "страта", "открыть", "open", "strat"
-Change weapon (Y button): "оружие", "weapon", "gun"
-Use item (A button): "use", "использовать", "юзать", "тык"
+left: ["left", "влево", 'a', 'ф'],
+right: ["right", "вправо", "grenade", "граната", 'd', 'в'],
+up: ["up", "вверх", "heal", "хил", 'w', 'ц'],
+down: ["down", "вниз", 'd', 'ы'],
+strategy: ["strat", "stratagem", "вызов", "страта", "открыть", "lb"],
+weapon: ["weapon", "оружие", "gun", "y"],
+use: ["use", "использовать", "юзать", "тык", "a"],
+reload: ["reload", "перезарядка", "x"],
+open_map: ["map", "карта"],
+aim: ["aim", "прицел", "пуль", "lt"],
+duck: ["duck", "присесть", "b"],
+None: ["back", "назад"]
 ```
